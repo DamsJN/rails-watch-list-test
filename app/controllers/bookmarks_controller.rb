51efc38d18a2fa4b5575c5d12d79d4c531b74887
damien jouaneau <damienjouaneau@gmail.com>
2021-11-11 22:31:15 +0100

    done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+
+  before_action :find_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark =Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+end
